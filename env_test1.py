c70240ad56f605c36a4c72dd332a856bb0ae7423
Reza <reza.abiri2020@gmail.com>
2020-06-25 20:21:41 -0700

    testing;  June25

--- env_test1.py
+++ env_test1.py
@@ -13,18 +13,24 @@ args = parser.parse_args()
 
 env = gym.make(args.env)
 
-while True:
+episode = 0
+while episode <1:
+    episode = episode +1
     done = False
     env.render()
     observation = env.reset()
+    
     action = env.action_space.sample()
     print('Observation size:', np.shape(observation), 'Action size:', np.shape(action))
-    print('next episoddddddddddddddddddddddddddddddddddddddddddddddddddddddddd')
+    print('next episoddddddddddddddddddddddddddddddddddddddddddddddddddddddddde')
     while not done:
         env.render()
-        observation, reward, done, info = env.step(env.action_space.sample())
+        #action = env.action_space.sample()
+        #print(action)
+        action=np.array([-100, -100, -100, -100, -100, -100, -100])
+        observation, reward, done, info = env.step(action)
         #print(observation)
-        print(reward)
-        print('next sampleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee point')
+        #print(reward)
+        #print('next sampleeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee point')
         #print(done)
         #print(info)
